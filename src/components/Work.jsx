34ca101cd50d1b19928499819707e19a79b25c78
Dusty-bun <deniskostow944@gmail.com>
2022-09-29 13:42:11 +0300

    projects cleared from work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -60,53 +60,6 @@ const Work = () => {
                </div>
               </div>
             </div>
-
-
-      {/* Grid Item */}
-      <div
-            style={{ backgroundImage: `url(${portfolio2})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                Portfolio
-              </span>
-              <div className='pt-8 text-center'>
-                <a href='https://github.com/Dusty-bun/portfolio' target='blank'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Inception
-                  </button>
-                  </a>
-               </div>
-              </div>
-            </div>
-
-
-
-                {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${sucert})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                Software University
-              </span>
-              <div className='pt-8 text-center'>
-                <a href='https://softuni.bg/certificates/details/141730/3652c93f' target='blank'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Original
-                  </button>
-                  </a>
-               </div>
-              </div>
-            </div>
-
-
-
-
          </div>
        </div>
       </div>
